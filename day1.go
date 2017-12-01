a9f517e9203373004379f8ce547d799093fab5bf
Lukasz Chmielewski <l.chmielewski@sportradar.com>
2017-12-01 07:29:13 +0100

    Day 1 part 2

--- day1.go
+++ day1.go
@@ -24,12 +24,17 @@ func main() {
 		instructions = strings.Split(line, "")
 	}
 
+	middle := len(instructions) / 2
+
 	var sum int64
+	var sumMiddle int64
 
 	for i := 0; i < len(instructions); i++ {
 		nextIndex := (i + 1) % len(instructions)
+		nextMiddleIndex := (i + middle) % len(instructions)
 		current, errCurrent := strconv.ParseInt(instructions[i], 10, 64)
 		next, errNext := strconv.ParseInt(instructions[nextIndex], 10, 64)
+		nextMiddle, errNextMiddle := strconv.ParseInt(instructions[nextMiddleIndex], 10, 64)
 
 		if errCurrent != nil {
 			log.Fatal(errCurrent)
@@ -39,12 +44,21 @@ func main() {
 			log.Fatal(errNext)
 		}
 
+		if errNextMiddle != nil {
+			log.Fatal(errNextMiddle)
+		}
+
 		if next == current {
 			sum += current
 		}
+
+		if nextMiddle == current {
+			sumMiddle += current
+		}
 	}
 
-	fmt.Println(sum)
+	fmt.Println("Part 1 solution: " + strconv.Itoa(int(sum)))
+	fmt.Println("Part 2 solution: " + strconv.Itoa(int(sumMiddle)))
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
